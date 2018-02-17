418248a6d388e1b3c10a0bc71602aa34eddfe4c0
Mark Omarov <inkfaust@gmail.com>
2018-02-17 19:34:28 +0200

    Tests of navigation | To refactor

--- spec/navigation.spec.js
+++ spec/navigation.spec.js
@@ -32,7 +32,7 @@ describe("Navigation", () => {
       const title = await page.title();
       expect(title).toBe("Solar Analyze | Home");
     },
-    16000
+    20000
   );
 
   test(
@@ -72,6 +72,84 @@ describe("Navigation", () => {
 
       expect(links).toEqual(expected);
     },
-    16000
+    20000
+  );
+
+  test(
+    "assert that redirect from home page is correctly",
+    async () => {
+      await page.goto(HOME);
+      await page.click(".main-navbar .main-menu li a[href='/']");
+      const titleHome = await page.title();
+      expect(titleHome).toBe("Solar Analyze | Home");
+
+      await page.goto(HOME);
+      await page.click(".main-navbar .main-menu li a[href='/about-us.html']");
+      const titleAboutUs = await page.title();
+      expect(titleAboutUs).toBe("Solar Analyze | About Us");
+
+      await page.goto(HOME);
+      await page.click(".main-navbar .main-menu li a[href='/faq.html']");
+      const titleFAQ = await page.title();
+      expect(titleFAQ).toBe("Solar Analyze | FAQ");
+
+      await page.goto(HOME);
+      await page.click(".main-navbar .main-menu li a[href='/contact-us.html']");
+      const titleContactUs = await page.title();
+      expect(titleContactUs).toBe("Solar Analyzer | Contact Us");
+
+      await page.goto(HOME);
+      await page.click(".main-navbar .main-menu li a[href='/form.html']");
+      const titleForm = await page.title();
+      expect(titleForm).toBe("Solar Analyze | Form");
+    },
+    20000
+  );
+
+  test(
+    "assert that copyrights text and href is correct",
+    async () => {
+      await page.goto(HOME);
+      const allResultsSelector = ".copyright a";
+
+      const links = await page.evaluate(allResultsSelector => {
+        const list = Array.from(document.querySelectorAll(allResultsSelector));
+
+        return list.map(link => {
+          const linkText = link.textContent.split("|")[0].trim();
+          return { text: linkText, href: link.href };
+        });
+      }, allResultsSelector);
+
+      const expected = [
+        {
+          text: "Privacy Policy",
+          href: "https://quote.solaranalyzer.com/privacy-policy.html"
+        },
+        {
+          text: "Terms of Use",
+          href: "https://quote.solaranalyzer.com/terms-of-use.html"
+        }
+      ];
+
+      expect(links).toEqual(expected);
+    },
+    20000
+  );
+
+  test(
+    "assert that privacy policy and terms of use redirect is correct",
+    async () => {
+      await page.goto(HOME);
+      await page.click(".copyright a[href='/privacy-policy.html']");
+      const titlePrivacy = await page.title();
+      expect(titlePrivacy).toBe("Solar Analyzer | Privacy Policy");
+
+      await page.goto(HOME);
+      await page.click(".copyright a[href='/terms-of-use.html']");
+      const titleTerms = await page.title();
+      expect(titleTerms).toBe("Solar Analyzer | Terms Of Use");
+    },
+    20000
   );
 });
